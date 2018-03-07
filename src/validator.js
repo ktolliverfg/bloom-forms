ebfef0f9bfd60cba503ea7a9f527b3e0c76856f1
Katie Foster <fosterkatiea@gmail.com>
2018-03-07 13:05:26 -0700

    removing console log

--- src/validator.js
+++ src/validator.js
@@ -41,7 +41,6 @@ export async function validatorAggregator(
 
   for (let field in testDataObject) {
     let thisField = testDataObject[field]
-    console.log('loop in validation', thisField)
     if (thisField.validateAs) {
       // multiple validateAs;
       if (thisField.validateAs.indexOf(' ') > -1) {
